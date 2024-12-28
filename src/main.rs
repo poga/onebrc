05ce8189ef9876883283732710470eec53dd8071
Poga Po <hi@devpoga.org>
2024-12-28 16:35:57 +0800

    final cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
-use std::collections::HashMap;
+use hashbrown::HashMap;
+use memchr::memchr;
 use std::env::args;
 use std::io::Read;
 
 struct Record {
     count: i32,
-    min: f32,
-    max: f32,
-    sum: f32,
+    min: i32,
+    max: i32,
+    sum: i32,
 }
 
 impl Record {
     fn default() -> Record {
         Record {
             count: 0,
-            min: f32::INFINITY,
-            max: f32::NEG_INFINITY,
-            sum: 0.0,
+            min: i32::MAX,
+            max: i32::MIN,
+            sum: 0,
         }
     }
 
-    fn add(&mut self, value: f32) {
+    fn add(&mut self, value: i32) {
         self.count += 1;
         self.sum += value;
         self.min = self.min.min(value);
         self.max = self.max.max(value);
     }
 
-    fn avg(&self) -> f32 {
-        self.sum / self.count as f32
+    fn avg(&self) -> i32 {
+        self.sum / self.count as i32
     }
 }
 
@@ -40,17 +41,57 @@ fn main() {
         .expect("Could not read file");
 
     let mut h = HashMap::new();
-    for line in data.lines() {
-        let (name, value) = line.split_once(';').unwrap();
-        let value: f32 = value.parse().expect("Could not parse value");
-        h.entry(name.to_string())
-            .or_insert(Record::default())
-            .add(value);
+    let mut data = data.as_bytes();
+    loop {
+        let Some(sep) = memchr(b';', data) else {
+            break;
+        };
+        let end = memchr(b'\n', &data[sep..]).unwrap();
+        let name = &data[..sep];
+        let value = &data[sep + 1..sep + end];
+        h.entry(name).or_insert(Record::default()).add(parse(value));
+        data = &data[sep + end + 1..];
     }
 
     let mut v: Vec<_> = h.iter().collect();
     v.sort_unstable_by_key(|p| p.0);
     for (name, r) in v {
-        println!("{name}: {:.1}/{:.1}/{:.1}", r.min, r.avg(), r.max);
+        println!(
+            "{}: {}/{}/{}",
+            String::from_utf8_lossy(name),
+            format(r.min),
+            format(r.avg()),
+            format(r.max)
+        );
     }
+}
+
+// parse into a fixed-precision i32 signed integer
+fn parse(mut s: &[u8]) -> i32 {
+    let neg = if s[0] == b'-' {
+        s = &s[1..];
+        true
+    } else {
+        false
+    };
+    // s = abc.d
+    let (a, b, c, d) = match s {
+        [c, b'.', d] => (0, 0, c - b'0', d - b'0'),
+        [b, c, b'.', d] => (0, b - b'0', c - b'0', d - b'0'),
+        [a, b, c, b'.', d] => (a - b'0', b - b'0', c - b'0', d - b'0'),
+        [c] => (0, 0, 0, c - b'0'),
+        [b, c] => (0, b - b'0', c - b'0', 0),
+        [a, b, c] => (a - b'0', b - b'0', c - b'0', 0),
+        _ => panic!("Unknown patters {:?}", std::str::from_utf8(s).unwrap()),
+    };
+    let v = a as i32 * 1000 + b as i32 * 100 + c as i32 * 10 + d as i32;
+    if neg {
+        -v
+    } else {
+        v
+    }
+}
+
+fn format(v: i32) -> String {
+    format!("{:.1}", v as f64 / 10.0)
 }
